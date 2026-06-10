2199ca1e0b86658de1891e0489600ddeeafb3627
k-omura <kosubadress0823@gmail.com>
2026-06-10 20:58:20 +0900

    fix parameter

--- python/parafitting_sim.py
+++ python/parafitting_sim.py
@@ -18,13 +18,13 @@ except ImportError:
 from scipy import signal
 from deap import base, creator, tools
 
-FRAME_SIZE    = 2048         # フレームサイズ
-SAMPLE_RATE   = 44100        # サンプリングレート
-MAX_EVAL_FREQ = 10000        # 10 kHz 以上を評価に含めない
-MEL_BANDS     = 64           # メルバンド数
+FRAME_SIZE    = 16384            # フレームサイズ
+SAMPLE_RATE   = 44100            # サンプリングレート
+MAX_EVAL_FREQ = 10000            # 10 kHz 以上を評価に含めない
 PLAYBACK_DURATION_SECONDS = 0.5  # パラメータ変更時に再生する音声長
-NGEN          = 1000           # GA世代数
-MELSPECTRUM_MODE = True           # メルスペクトラムを使用するかどうか
+NGEN          = 1000             # GA世代数
+MELSPECTRUM_MODE = True          # メルスペクトラムを使用するかどうか
+MEL_BANDS     = 64               # メルバンド数
 
 class GApara(IntEnum):
     NO = 0,
@@ -41,7 +41,7 @@ class MidiMsg(IntEnum):
 
 class MidiCC(IntEnum):
     BANKSLECT = 0,               # Allows user to switch bank for patch selection. Program change used with Bank Select. MIDI can access 16,384 patches per MIDI channel.
-    MODWHELL = 1,			    # Generally this CC controls a vibrato effect (pitch, loudness, brighness). What is modulated is based on the patch.
+    MODWHELL = 1,			     # Generally this CC controls a vibrato effect (pitch, loudness, brighness). What is modulated is based on the patch.
     BREATHCONT = 2,              # Oftentimes associated with aftertouch messages. It was originally intended for use with a breath MIDI controller in which blowing harder produced higher MIDI control values. It can be used for modulation as well.
     FOOTCONT = 4,                # Often used with aftertouch messages. It can send a continuous stream of values based on how the pedal is used.
     PORTAMENTOTIME = 5,          # Controls portamento rate to slide between 2 notes played subsequently.
@@ -57,23 +57,23 @@ class MidiCC(IntEnum):
     GENERAL18 = 18,              #
     GENERAL19 = 19,              #
     USER_ATTACKLEVEL = 20,       #
-    USER_WAVE1SINEAMP = 21,		#
-    USER_WAVE1SQUAMP = 22,		#
-    USER_WAVE1SQUDUTY = 23,		#
-    USER_WAVE1TRIAMP = 24,		#
-    USER_WAVE1TRIPEAK = 25,		#
-    USER_WAVE1PITCH = 26,		#
-    USER_WAVE1OCTAVE = 27,		#
-    USER_WAVE2SINEAMP = 28,		#
-    USER_WAVE2SQUAMP = 29,		#
-    USER_WAVE2SQUDUTY = 30,		#
-    USER_WAVE2TRIAMP = 31,		#
-    USER_WAVE2TRIPEAK = 32,	    #
-    USER_WAVE2PITCH = 33,   	    #
-    USER_WAVE2OCTAVE = 34,	    #
+    USER_WAVE1SINEAMP = 21,		 #
+    USER_WAVE1SQUAMP = 22,		 #
+    USER_WAVE1SQUDUTY = 23,		 #
+    USER_WAVE1TRIAMP = 24,		 #
+    USER_WAVE1TRIPEAK = 25,		 #
+    USER_WAVE1PITCH = 26,		 #
+    USER_WAVE1OCTAVE = 27,		 #
+    USER_WAVE2SINEAMP = 28,		 #
+    USER_WAVE2SQUAMP = 29,		 #
+    USER_WAVE2SQUDUTY = 30,		 #
+    USER_WAVE2TRIAMP = 31,		 #
+    USER_WAVE2TRIPEAK = 32,	     #
+    USER_WAVE2PITCH = 33,        #
+    USER_WAVE2OCTAVE = 34,	     #
     USER_DISTORTION_LEVEL = 35,  #
     USER_PITCHBYNOTE = 36,       #
-    USER_FILTERTYPE = 37,#
+    USER_FILTERTYPE = 37,        #
     USER_CH0MIRRORCH = 38,       #
     USER_TRE_TYPE = 39,          #
     USER_TRE_PEAK = 40,          #
@@ -86,18 +86,18 @@ class MidiCC(IntEnum):
     USER_WOW_PEAK = 47,          #
     USER_WOW_AMP = 48,           #
     USER_WOW_FREQ = 49,          #
-    USER_ENV_VOL_DIFF = 50,		#
-    USER_ENV_VOL_TIME = 51,		#
-    USER_ENV_FREQ_DIFF = 52,		#
-    USER_ENV_FREQ_TIME = 53,		#
-    USER_ENV_WOW_DIFF = 54,		#
-    USER_ENV_WOW_TIME = 55,		#
-    USER_DRUM_NN = 56,			#
-    USER_DRUM_TYPE = 57,		    #
-    USER_DRUM_LPF_FCNN = 58,		#
-    USER_DRUM_HPF_FCNN = 59,		#
-    USER_DRUM_LPF_Q = 60,		#
-    USER_DRUM_HPF_Q = 61,		#
+    USER_ENV_VOL_DIFF = 50,		 #
+    USER_ENV_VOL_TIME = 51,		 #
+    USER_ENV_FREQ_DIFF = 52,     #
+    USER_ENV_FREQ_TIME = 53,	 #
+    USER_ENV_WOW_DIFF = 54,		 #
+    USER_ENV_WOW_TIME = 55,		 #
+    USER_DRUM_NN = 56,			 #
+    USER_DRUM_TYPE = 57,	     #
+    USER_DRUM_LPF_FCNN = 58,   	 #
+    USER_DRUM_HPF_FCNN = 59,	 #
+    USER_DRUM_LPF_Q = 60,		 #
+    USER_DRUM_HPF_Q = 61,		 #
     DAMPERPEDAL = 64,            # ≤63 off, ≥64 on. On/off switch that controls sustain pedal. Nearly every synth will react to CC 64. (See also Sostenuto CC 66)
     PORTAMENTO = 65,             # ≤63 off, ≥64 on. On/off switch
     SOSTENTOPEDAL = 66,          # ≤63 off, ≥64 on. On/off switch – Like the Sustain controller (CC 64), However, it only holds notes that were “On” when the pedal was pressed. People use it to “hold” chords” and play melodies over the held chord.
@@ -106,13 +106,13 @@ class MidiCC(IntEnum):
     HOLD2 = 69,                  # ≤63 off, ≥64 on	Another way to “hold notes” (see MIDI CC 64 and MIDI CC 66). However notes fade out according to their release parameter rather than when the pedal is released.
     SOUNDCONT1 = 70,             # Usually controls the way a sound is produced. Default =  Sound Variation.
     SOUNDCONT2 = 71,             # Allows shaping the Voltage Controlled Filter (VCF). Default =  Resonance also (Timbre or Harmonics)
-    USER_FILTERQFACTOR = 71,        # Resonance of the filter(q factor)
+    USER_FILTERQFACTOR = 71,     # Resonance of the filter(q factor)
     SOUNDCONT3 = 72,             # Controls release time of the Voltage controlled Amplifier (VCA). Default =  Release Time.
     USER_RELEASETIME = 72,       # Release Time
     SOUNDCONT4 = 73,             # Controls the “Attack’ of a sound. The attack is the amount of time it takes for the sound to reach maximum amplitude.
     USER_ATTACKTIME = 73,        # Controls the “Attack’ of a sound. The attack is the amount of time it takes for the sound to reach maximum amplitude.
     SOUNDCONT5 = 74,             # Controls VCFs cutoff frequency of the filter.
-    USER_FILTERCUTOFF = 74,         # cutoff frequency of the filter
+    USER_FILTERCUTOFF = 74,      # cutoff frequency of the filter
     GENERAL75 = 75,              # Generic – Some manufacturers may use to further shave their sounds.
     GENERAL76 = 76,              # Generic – Some manufacturers may use to further shave their sounds.
     GENERAL77 = 77,              # Generic – Some manufacturers may use to further shave their sounds.
@@ -138,23 +138,23 @@ class MidiCC(IntEnum):
     NRPNMSB = 99,                # For controllers 6, 38, 96, and 97, it selects the NRPN parameter.
     RPNLSB = 100,                # For controllers 6, 38, 96, and 97, it selects the RPN parameter.
     RPNMSB = 101,                # For controllers 6, 38, 96, and 97, it selects the RPN parameter.
-    USER_MTRE_TYPE = 102,		#
-    USER_MTRE_PEAK = 103,		#
-    USER_MTRE_AMP = 104,	        #
-    USER_MTRE_FREQ = 105,		#
-    USER_MVIB_TYPE = 106,		#
-    USER_MVIB_PEAK = 107,		#
-    USER_MVIB_AMP = 108,			#
-    USER_MVIB_FREQ = 109,		#
-    USER_MWOW_TYPE = 110,		#
-    USER_MWOW_PEAK = 111,		#
-    USER_MWOW_AMP = 112,			#
-    USER_MWOW_FREQ = 113,		#
-    USER_MFILTER_TYPE = 114,		#
-    USER_MFILTER_QFACTOR = 115,	#
-    USER_MFILTER_CUTOFF = 116,	#
+    USER_MTRE_TYPE = 102,		 #
+    USER_MTRE_PEAK = 103,		 #
+    USER_MTRE_AMP = 104,	     #
+    USER_MTRE_FREQ = 105,		 #
+    USER_MVIB_TYPE = 106,		 #
+    USER_MVIB_PEAK = 107,		 #
+    USER_MVIB_AMP = 108,	 	 #
+    USER_MVIB_FREQ = 109,		 #
+    USER_MWOW_TYPE = 110,		 #
+    USER_MWOW_PEAK = 111,		 #
+    USER_MWOW_AMP = 112,		 #
+    USER_MWOW_FREQ = 113,		 #
+    USER_MFILTER_TYPE = 114,	 #
+    USER_MFILTER_QFACTOR = 115,	 #
+    USER_MFILTER_CUTOFF = 116,	 #
     USER_PRESET = 118,           # Preset
-    USER_MODECONTROL = 119,       # 
+    USER_MODECONTROL = 119,      # 
     ALLSOUNDOFF = 120,           # Mutes all sound. It does so regardless of release time or sustain. (See MIDI CC 123)
     RESETALL = 121,              # It will reset all controllers to their default.
     LOCALSWITCH = 122,           # Turns internal connection of a MIDI keyboard or workstation, etc. on or off. If you use a computer, you will most likely want local control off to avoid notes being played twice. Once locally and twice when the note is sent back from the computer to your keyboard.
@@ -652,7 +652,7 @@ def main():
         Parameter("synth-tri1-peak", GApara.YES, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1TRIPEAK, 0, 127, 0),
         Parameter("synth-squ1-volume", GApara.YES, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1SQUAMP, 0, 127, 0),
         Parameter("synth-squ1-duty", GApara.YES, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1SQUDUTY, 0, 127, 64),
-        Parameter("synth-pitch1", GApara.YES, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1PITCH, 0, 127, 64),
+        Parameter("synth-pitch1", GApara.NO, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1PITCH, 0, 127, 64),
         Parameter("synth-octave-shift1", GApara.NO, 0, MidiMsg.CONTROLCHANGE, MidiCC.USER_WAVE1OCTAVE, 40, 88, 64),
 
         # Waveform 2
